a7d376228aa9d0a155d906391357aba3374254a1
Jun Liang <illuminatemynight@gmail.com>
2019-12-13 19:06:30 -0600

    first commit

--- app.js
+++ app.js
@@ -4,12 +4,42 @@ const keySecret = process.env.SECRET_KEY;
 const express = require("express");
 const stripe = require("stripe")(keySecret);
 const bodyParser = require("body-parser");
-
 const app = express();
-app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use('/api', getAllPlans)
+
+const req = {};
+const res = {
+    json : function(payload){
+        console.log("All Stripe Plans:")
+        for(let plan of payload.data){
+            console.log(`Plan ${plan.id}, Name: ${plan.product.id}, Amount: ${plan.amount/100}/${plan.interval}`)
+        }
+        console.log("payload:", payload)
+    }
+};
+const next = function(){};
+
+async function getAllPlans(req, res, next){
+    const plans = await stripe.plans.list({expand: ["data.product"]});
+    res.json(plans)
+}
+
+getAllPlans(req, res, next);
+
+app.get('/home', (req, res) => {
+    res.send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About');
+});
+
+app.get('/books/:bookId', (req, res) => {
+    res.send(req.params);
+});
 
 app.post("/charge", (req, res) => {
     let amount = 10000;
